1f04ee5e9aa9e6e21ecb5e460d610d97d2abf229
Rene van Aerle <info@rvgate.nl>
2013-06-19 12:22:36 +0200

    Enabled download and ignore final deb package

--- package.sh
+++ package.sh
@@ -1,4 +1,6 @@
 #!/bin/sh 
+set -x
+
 PACKAGE_NAME="lswpentaho"
 PACKAGE_VERSION="1.0"
 
@@ -12,16 +14,16 @@ RESOURCES="$SCRIPTPATH/resources"
 
 # ==========================================================
 # REMOVE OLD PACKAGE
-#rm "${PACKAGE_FULLNAME}.deb"
+rm "${PACKAGE_FULLNAME}.deb"
 rm "$PACKAGE_ROOT" -Rf
 # ==========================================================
 
 # ==========================================================
 # DOWNLOAD PENTAHO
-#wget http://downloads.sourceforge.net/project/pentaho/Business%20Intelligence%20Server/4.8.0-stable/biserver-ce-4.8.0-stable.tar.gz
-#wget http://downloads.sourceforge.net/project/pentaho/Data%20Integration/4.4.0-stable/pdi-ce-4.4.0-stable.tar.gz
-#tar -xvf biserver-ce-4.8.0-stable.tar.gz
-#tar -xvf pdi-ce-4.4.0-stable.tar.gz
+wget http://downloads.sourceforge.net/project/pentaho/Business%20Intelligence%20Server/4.8.0-stable/biserver-ce-4.8.0-stable.tar.gz
+wget http://downloads.sourceforge.net/project/pentaho/Data%20Integration/4.4.0-stable/pdi-ce-4.4.0-stable.tar.gz
+tar -xvf biserver-ce-4.8.0-stable.tar.gz
+tar -xvf pdi-ce-4.4.0-stable.tar.gz
 # ==========================================================
 
 
@@ -51,25 +53,37 @@ cp "$RESOURCES/upstart/pentaho.conf" "$PACKAGE_ROOT/etc/init/"
 # ==========================================================
 # Apply patches for biserver-ce
 cd "$PACKAGE_ROOT/opt/pentaho/biserver-ce/"
-git apply "$RESOURCES/patches/biserver-ce/0001-Copied-MySQL-driver-from-administration-console.patch"
-git apply "$RESOURCES/patches/biserver-ce/0002-Converted-to-MySQL.patch"
-git apply "$RESOURCES/patches/biserver-ce/0003-Disabled-Hypersonic-automatic-startup.patch"
-git apply "$RESOURCES/patches/biserver-ce/0004-Converted-Quartz-to-MySQL.patch"
-git apply "$RESOURCES/patches/biserver-ce/0005-Disabled-evaluation-login.patch"
-git apply "$RESOURCES/patches/biserver-ce/0006-Disabled-HSQL-startup-listener.patch"
-git apply "$RESOURCES/patches/biserver-ce/0007-Added-original-config-files.patch"
-git apply "$RESOURCES/patches/biserver-ce/0008-Added-initial-repository-and-quartz-databases.patch"
-git apply "$RESOURCES/patches/biserver-ce/0009-Removed-global-department-session-startup-action.patch"
+echo "Patch biserver-ce 1/9"
+cp "$PACKAGE_ROOT/opt/pentaho/administration-console/jdbc/mysql-connector-java-5.1.17.jar" "$PACKAGE_ROOT/opt/pentaho/biserver-ce/tomcat/lib/"
+#patch -p1 < "$RESOURCES/patches/biserver-ce/0001-Copied-MySQL-driver-from-administration-console.patch"
+
+echo "Patch biserver-ce 2/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0002-Converted-to-MySQL.patch"
+echo "Patch biserver-ce 3/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0003-Disabled-Hypersonic-automatic-startup.patch"
+echo "Patch biserver-ce 4/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0004-Converted-Quartz-to-MySQL.patch"
+echo "Patch biserver-ce 5/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0005-Disabled-evaluation-login.patch"
+echo "Patch biserver-ce 6/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0006-Disabled-HSQL-startup-listener.patch"
+echo "Patch biserver-ce 7/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0007-Added-original-config-files.patch"
+echo "Patch biserver-ce 8/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0008-Added-initial-repository-and-quartz-databases.patch"
+echo "Patch biserver-ce 9/9"
+patch -p1 < "$RESOURCES/patches/biserver-ce/0009-Removed-global-department-session-startup-action.patch"
 
 # Apply patches for administration-console
 cd "$PACKAGE_ROOT/opt/pentaho/administration-console/"
-git apply "$RESOURCES/patches/administration-console/0001-Added-original-config-files.patch"
+echo "Patch administration-console 1/1"
+patch -p1 < "$RESOURCES/patches/administration-console/0001-Added-original-config-files.patch"
 
 # Apply patches for data-integration
 cd "$PACKAGE_ROOT/opt/pentaho/data-integration/"
-git apply "$RESOURCES/patches/data-integration/0001-Added-mysql-library-from-administration-console.patch"
-
-exit
+echo "Patch data-integration 1/1"
+cp "$PACKAGE_ROOT/opt/pentaho/administration-console/jdbc/mysql-connector-java-5.1.17.jar" "$PACKAGE_ROOT/opt/pentaho/data-integration/libext/"
+#patch -p1 < "$RESOURCES/patches/data-integration/0001-Added-mysql-library-from-administration-console.patch"
 # ==========================================================
 
 
@@ -93,6 +107,5 @@ chmod 0775 "$PACKAGE_ROOT/DEBIAN/config"
 # BUILD DEBIAN PACKAGE
 # ==========================================================
 cd "$PACKAGE_ROOT/../"
-exit
 dpkg-deb --build "$PACKAGE_ROOT"
 # ==========================================================
